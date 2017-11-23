65b5863d571f78679ff151254acab8de7ffd1ac7
Gideon Paul <gidzpaul95@gmail.com>
2017-11-23 11:53:00 -0500

    Basic Timing has been implemented

--- src/Server.java
+++ src/Server.java
@@ -4,16 +4,44 @@ import java.net.Socket;
 
 public class Server {
     static final int PORT = 8080;
+    static final long endTime = 5;
+    static final String exitMessage = "The Elections have ended. No further votes will be accepted";
+
+    static long start = System.currentTimeMillis();
+
     public static void main(String[] args)
             throws IOException {
         ServerSocket s = new ServerSocket(PORT);
         System.out.println("Server Started");
         try {
+
+            /* The timing mechanism */
+            /////////////////////////
+
+            Thread t = new Thread() {
+                public void run() {
+                    long end = start + (endTime * 1000); // 60 seconds * 1000 ms/sec
+                    int i=0;
+                    while (System.currentTimeMillis() < end)
+                    {
+                        //Busy wait
+                    }
+                    System.out.println(exitMessage);
+                    System.exit(0);
+
+                }
+            };
+            t.start();
+
+            //////////////////////////
+            /* The timing mechanism */
+
+
             while(true) {
                 // Blocks until a connection occurs:
                 Socket socket = s.accept();
                 try {
-                    new ServerThread(socket);
+                        new ServerThread(socket);
                 } catch(IOException e) {
                     // If it fails, close the socket,
                     // otherwise the thread will close it:
@@ -21,6 +49,7 @@ public class Server {
                 }
             }
         } finally {
+            System.out.println("The server is shutting down");
             s.close();
         }
     }
